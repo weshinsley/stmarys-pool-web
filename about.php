e2ba6027002665a3c1228843514a5bd811f68866
Wes Hinsley <w.hinsley@imperial.ac.uk>
2018-11-02 18:58:38 +0000

    About stub

---
+++ about.php
@@ -0,0 +1,33 @@
+<?php
+ 
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <style>
+
+<?php  
+  readfile("styles.css");
+  
+?>
+    </style>
+    <script language="JavaScript">
+    </script>
+  </head>
+  <body><center>
+    <table cellpadding="0" cellspacing="2">
+      <tr><td><img src="images/ic.png" alt="" width="165" height="63"/></td>
+          <td><img src="images/sm.png" alt="" width="334" height="63"/></td>
+          <td><img src="images/si.png" alt="" width="165" height="63"/></td>
+      </tr>
+     <tr><td colspan="2" style="padding:2px; font-family:Calibri,Arial;font-size:14px;color:#ffffff; background:#00558E">
+        &nbsp;&nbsp;&nbsp;<a href="index.php">Current Status</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;<a href="timetable.php">Timetable</a>&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;<U>About</u></td><td style="padding:2px; font-family:Calibri,Arial;font-size:14px;color:#ffffff;text-align:right; background:#00558E">
+        <a href="manage/">Management</a>&nbsp;&nbsp;&nbsp;</span></td></tr>
+        
+        <tr><td style="background:url('images/bg.png')" colspan="3"><center>&nbsp;<br/>
+      
+    &nbsp;<br/>
+    </center></td></tr></table>
+  </center></body>
+</html>
